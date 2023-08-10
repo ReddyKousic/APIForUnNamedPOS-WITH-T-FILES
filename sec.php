f3febc3981eb8037805f250c6890d416e18bd16a
ReddyKousic <85285392+ReddyKousic@users.noreply.github.com>
2023-08-10 15:06:34 +0530

    main

--- sec.php
+++ sec.php
@@ -3,12 +3,17 @@ $_ENV['key'] = "kousic";
 $_ENV['getp'] = "kousic111";
 $_ENV['skc'] = "kousic";
 
+$_ENV['host'] = 'localhost';
+$_ENV['username'] = 'root';
+$_ENV['password'] = '';
+$_ENV['database'] = 'apihub';
 
 
-$_ENV['host'] = 'mysql-unnames-pos-beta-test-rel-601.alwaysdata.net';
-$_ENV['username'] = '323579';
-$_ENV['password'] = 'kousic2211';
-$_ENV['database'] = 'unnames-pos-beta-test-rel-601_base';
+
+// $_ENV['host'] = 'mysql-unnames-pos-beta-test-rel-601.alwaysdata.net';
+// $_ENV['username'] = '323579';
+// $_ENV['password'] = 'kousic2211';
+// $_ENV['database'] = 'unnames-pos-beta-test-rel-601_base';
 
 
 ?>
